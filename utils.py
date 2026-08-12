9873823308394eb65c2573fcc22a927eed16f3da
Farangis <farangisnabavi@gmail.com>
2026-08-12 20:11:39 +0330

    search by name
    search item by their name function added

--- utils.py
+++ utils.py
@@ -23,3 +23,9 @@ def edit_products(id, name, category, price, stock, looking_for_id, looking_for_
         for item in productList:
             if looking_for_id in item['id'] and looking_for_name in item['name']:
                 item.update({"id": id, "name": name, "category": category, "price": price, "stock": stock})
+def search_products_by_name(name):
+    #this function searches products by their name
+    with open(data/products.json, 'r', encoding='utf-8') as productList:
+        for item in productList:
+            if name in item['name']:
+                print("id: ", item['id'],"\nname: ",item['name'],"\ncategory: ", item['category'],"\nprice: ", item['price'],"\nstock: ", item['stock'])
