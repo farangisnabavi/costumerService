b2507fe53a93e4c74c2b1687a69738ed78126ad2
Farangis <farangisnabavi@gmail.com>
2026-08-12 19:27:17 +0330

    delete products
    delete products functions added

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+#====================================================
+#                    PRODUCTS
+#====================================================
 def add_products(id,name,category,price,stock):
+    #this function adds products
     with open(data/products.json, 'a', encoding='utf-8') as productList:
         productList.append({
             'id': id,
@@ -7,3 +11,9 @@ def add_products(id,name,category,price,stock):
             'price': price,
             'stock': stock
         })
+def remove_products(id, name):
+    #this function deletes products
+    with open(data/products.json, 'rw', encoding='utf-8') as productList:
+       for item in productList:
+           if id in item['id'] and name in item['name']:
+               productList.remove(item)
