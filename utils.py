07f4c6e32c5ff7d56881584251d2d5c5b34d74e5
Farangis <farangisnabavi@gmail.com>
2026-08-12 19:31:32 +0330

    edit products
    edit products function added

--- utils.py
+++ utils.py
@@ -17,3 +17,9 @@ def remove_products(id, name):
        for item in productList:
            if id in item['id'] and name in item['name']:
                productList.remove(item)
+def edit_products(id, name, category, price, stock, looking_for_id, looking_for_name):
+   #this function edits products
+    with open(data/products.json, 'rw', encoding='utf-8') as productList:
+        for item in productList:
+            if looking_for_id in item['id'] and looking_for_name in item['name']:
+                item.update({"id": id, "name": name, "category": category, "price": price, "stock": stock})
