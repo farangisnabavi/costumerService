a65b7c290437b7f493778b3d0e8624d05130a107
Farangis <farangisnabavi@gmail.com>
2026-08-14 18:04:09 +0330

    bugs fixed
    fixes bugs relating to json file load

--- utils.py
+++ utils.py
@@ -1,50 +1,83 @@
-#====================================================
+import json
+
+
+# ====================================================
 #                    PRODUCTS
-#====================================================
-def add_products(id,name,category,price,stock):
-    #this function adds products
-    with open(data/products.json, 'a', encoding='utf-8') as productList:
-        productList.append({
+# ====================================================
+def add_products(id, name, category, price, stock):
+    # this function adds products
+    with open("data/products.json", 'a', encoding='utf-8') as f:
+        product_list = json.load(f)
+    product_list.append({
             'id': id,
             'name': name,
             'category': category,
             'price': price,
             'stock': stock
-        })
+    })
+    with open("data/products.json", 'w', encoding='utf-8') as f:
+        json.dump(product_list, f, ensure_ascii=False, indent=4)
+
+
 def remove_products(id, name):
-    #this function deletes products
-    with open(data/products.json, 'rw', encoding='utf-8') as productList:
-       for item in productList:
-           if id in item['id'] and name in item['name']:
-               productList.remove(item)
+    # this function deletes products
+    with open("data/products.json", 'rw', encoding='utf-8') as f:
+        product_list = json.load(f)
+    for item in product_list:
+        if id in item['id'] and name in item['name']:
+            product_list.remove(item)
+    with open("data/products.json", 'w', encoding='utf-8') as f:
+        json.dump(product_list, f, ensure_ascii=False, indent=4)
+
+
 def edit_products(id, name, category, price, stock, looking_for_id, looking_for_name):
-   #this function edits products
-    with open(data/products.json, 'rw', encoding='utf-8') as productList:
-        for item in productList:
+    # this function edits products
+    with open("data/products.json", 'rw', encoding='utf-8') as f:
+        product_list = json.load(f)
+        for item in product_list:
             if looking_for_id in item['id'] and looking_for_name in item['name']:
                 item.update({"id": id, "name": name, "category": category, "price": price, "stock": stock})
+                json.dump(product_list, f, ensure_ascii=False, indent=4)
+
+
 def search_products_by_name(name):
-    #this function searches products by their name
-    with open(data/products.json, 'r', encoding='utf-8') as productList:
-        for item in productList:
+    # this function searches products by their name
+    with open("data/products.json", 'r', encoding='utf-8') as f:
+        product_list = json.load(f)
+        for item in product_list:
             if name in item['name']:
-                print("id: ", item['id'],"\nname: ",item['name'],"\ncategory: ", item['category'],"\nprice: ", item['price'],"\nstock: ", item['stock'])
+                print("id: ", item['id'], "\nname: ", item['name'], "\ncategory: ", item['category'], "\nprice: ",
+                      item['price'], "\nstock: ", item['stock'])
+
+
 def search_products_by_category(category):
-    #this function searches products by their category
-    with open(data/products.json, 'r', encoding='utf-8') as productList:
-        for item in productList:
+    # this function searches products by their category
+    with open("data/products.json", 'r', encoding='utf-8') as f:
+        product_list = json.load(f)
+        for item in product_list:
             if category in item['category']:
-                print("id: ", item['id'], "\nname: ", item['name'], "\ncategory: ", item['category'], "\nprice: ",item['price'], "\nstock: ", item['stock'])
+                print("id: ", item['id'], "\nname: ", item['name'], "\ncategory: ", item['category'], "\nprice: ",
+                      item['price'], "\nstock: ", item['stock'])
+
+
 def show_products():
-    #this function prints products
-    with open(data/products.json, 'r', encoding='utf-8') as productList:
-        for item in productList:
-            print("id: ", item['id'],"\nname: ",item['name'],"\ncategory: ", item['category'],"\nprice: ", item['price'],"\nstock: ", item['stock'])
+    # this function prints products
+    with open("data/products.json", 'r', encoding='utf-8') as f:
+        product_list = json.load(f)
+        for item in product_list:
+            print("id: ", item['id'], "\nname: ", item['name'], "\ncategory: ", item['category'], "\nprice: ",
+                  item['price'], "\nstock: ", item['stock'])
+
+
 def sort_by_price():
-    #this function sortes by price
-    with open(data/products.json, 'r', encoding='utf-8') as productList:
-        print(sorted(productList, key=lambda x: x["price"]))
+    # this function sortes by price
+    with open("data/products.json", 'r', encoding='utf-8') as f:
+        product_list = json.load(f)
+        print(sorted(product_list, key=lambda x: x["price"]))
+
+
 def sort_by_stock():
-    #this function sortes by stock
-    with open(data/products.json, 'r', encoding='utf-8') as productList:
-        print(sorted(productList, key=lambda x: x["stock"]))
+    # this function sortes by stock
+    with open("data/products.json", 'r', encoding='utf-8') as f:
+        product_list = json.load(f)
+        print(sorted(product_list, key=lambda x: x["stock"]))
