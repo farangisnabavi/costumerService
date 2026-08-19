2579065785dd1557ca1f25a4d024082d6e991408
Farangis <farangisnabavi@gmail.com>
2026-08-19 11:20:16 +0330

    stock worth added
    stock worth function added
    uses map, reduce, lambda

---
+++ reports.py
@@ -0,0 +1,18 @@
+from functools import reduce
+import utils
+productPath = "data/products.json"
+ordersPath = "data/orders.json"
+usersPath = "data/users.json"
+
+def stock_worth():
+    #calculates the stock worth
+    products = utils.load_file(productPath)
+    product_stock = []
+    product_price = []
+    for product in products:
+        product_stock.append(product["stock"])
+        product_price.append(product["price"])
+    stocks_worth = list(map(lambda x,y: x*y, product_stock, product_price))
+    final_worth = reduce(lambda x,y: x+y, stocks_worth)
+    print(final_worth)
+
